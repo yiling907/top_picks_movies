3517ecc20ef21a713b6e892c23b107dcc41f7ecb
yiling <yilinglei907@gmail.com>
2025-12-03 16:54:16 +0000

    fix: fix the terraform config

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,18 +1,18 @@
 locals {
-  bucket_name = "django-ecs-ecr-terraform-1"
-  table_name  = "django-ecs-ecr-terraform-1"
+  bucket_name = "django-music-ecs-ecr-terraform-1"
+  table_name  = "django-music-ecs-ecr-terraform-1"
 
-  ecr_repo_name = "django-app-ecr-repo"
+  ecr_repo_name = "django--music-app-ecr-repo"
 
-  music_app_cluster_name        = "django-app-cluster"
-  availability_zones           = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
-  music_app_task_famliy         = "django-app-task"
+  music_app_cluster_name        = "django-music-app-cluster"
+  availability_zones           = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  music_app_task_famliy         = "django-music-app-task"
   container_port               = 8000
-  music_app_task_name           = "django-app-task"
-  ecs_task_execution_role_name = "django-app-task-execution-role"
+  music_app_task_name           = "django-music-app-task"
+  ecs_task_execution_role_name = "django-music-app-task-execution-role"
 
-  application_load_balancer_name = "django-app-alb"
-  target_group_name              = "django-alb-tg"
+  application_load_balancer_name = "django-music-app-alb"
+  target_group_name              = "django-music-alb-tg"
 
-  music_app_service_name = "django-app-service"
+  music_app_service_name = "django-music-app-service"
 }
